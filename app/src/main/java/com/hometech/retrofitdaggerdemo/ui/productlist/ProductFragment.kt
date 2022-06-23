b114b41fbac3f7ded2aeb5e7c7374b987755cb33
Arpit Patel <arpit@temi.ca>
2022-06-23 12:18:20 -0400

    Attach view binding to fragment.

--- app/src/main/java/com/hometech/retrofitdaggerdemo/ui/productlist/ProductFragment.kt
+++ app/src/main/java/com/hometech/retrofitdaggerdemo/ui/productlist/ProductFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hometech.retrofitdaggerdemo.R
+import com.hometech.retrofitdaggerdemo.databinding.FragmentProductBinding
 
 class ProductFragment : Fragment() {
 
     private lateinit var viewModel: ProductViewModel
 
+    private lateinit var binding: FragmentProductBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_product, container, false)
+    ): View {
+
+        binding = FragmentProductBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
