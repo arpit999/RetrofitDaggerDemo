8cb36f3d0b5a8c6e062f570e0557c3292f4af412
Arpit Patel <arpit@temi.ca>
2022-06-23 12:02:28 -0400

    Set ViewBinding in MainActivity.kt

--- app/src/main/java/com/hometech/retrofitdaggerdemo/MainActivity.kt
+++ app/src/main/java/com/hometech/retrofitdaggerdemo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hometech.retrofitdaggerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hometech.retrofitdaggerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
